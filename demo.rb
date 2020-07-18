ed37268ed02d8cebe32824eee4f6e6c33306e4c3
Javier Goncalves <jagi.ccs@gmail.com>
2020-07-18 18:48:56 +0000

    Automatically backed up by Learn

---
+++ demo.rb
@@ -0,0 +1,22 @@
+#greetings = "friendly_greeting"
+#case greetings
+#  when "unfriendly_greeting"
+#    puts "fuck you"
+#  when "friendly_greeting"
+#    puts "Hello"
+#  end
+
+magic_exit_number = 7
+count = 0
+while count < 10 do
+  break if count == magic_exit_number
+  puts "I am the #{count}, I love to count!" # Work
+  count = count + 1
+end
+
+magic_exit_number = 7
+count = 0
+while count < 10 && count != magic_exit_number do
+  puts "I am the #{count}, I love to count!" # Work
+  count = count + 1
+end
